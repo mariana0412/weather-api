115789f00236c61a35bf1436319055abeced27e3
Mariana Piz <marianapiz0412@gmail.com>
2025-05-19 21:23:55 +0300

    Separate db and mailer configurations into separate files

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('./config/db');
 const weatherRoutes = require('./routes/weather');
 const subscriptionRoutes = require('./routes/subscription');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use((req, res, next) => {
     console.log(`→ ${req.method} ${req.url}`);
     next();
